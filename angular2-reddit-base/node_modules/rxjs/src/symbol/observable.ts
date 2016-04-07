5a2c8182990831b875c93a6b410e700803045360
Gerben Rampaart <Gerben Rampaart>
2016-04-07 19:35:42 +0200

    git

---
+++ angular2-reddit-base/node_modules/rxjs/src/symbol/observable.ts
@@ -0,0 +1,21 @@
+/// <reference path="./symbol.d.ts" />
+import {root} from '../util/root';
+
+const Symbol = root.Symbol;
+
+export let $$observable: symbol;
+
+if (typeof Symbol === 'function') {
+  if (Symbol.observable) {
+    $$observable = Symbol.observable;
+  } else {
+    if (typeof Symbol.for === 'function') {
+      $$observable = Symbol.for('observable');
+    } else {
+      $$observable = Symbol('observable');
+    }
+    Symbol.observable = $$observable;
+  }
+} else {
+  $$observable = <any>'@@observable';
+}
---
+++ angular2-reddit-base/node_modules/rxjs/symbol/observable.d.ts
@@ -0,0 +1,2 @@
+/// <reference path="../src/symbol/symbol.d.ts" />
+export declare let $$observable: symbol;
